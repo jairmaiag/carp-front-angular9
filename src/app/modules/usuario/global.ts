472d323d2bd34e277aec949c2138b246158affc7
jairmaiag <jairmaiag@gmail.com>
2020-07-16 18:18:25 -0300

    Dados globais do módulo de usuário

---
+++ src/app/modules/usuario/global.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Global {
+    modulo: String = 'usuario';
+    titulo: String = 'Usuário';
+    inclusao: String = 'Inclusão de ';
+    alteracao: String = 'Alteração de ';
+    detalhe: String = 'Detalhe do ';
+    listagem: String = 'Listagem de ';
+}
