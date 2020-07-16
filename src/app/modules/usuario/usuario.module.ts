73fb9db5ada6edb73122126516e486878fe54297
jairmaiag <jairmaiag@gmail.com>
2020-07-16 13:57:52 -0300

    Inclusão do módulo de rotas no módulo de usuário

--- src/app/modules/usuario/usuario.module.ts
+++ src/app/modules/usuario/usuario.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { UsuarioRoutingModule } from './usuario-routing.module';
 import { UsuarioFormComponent } from './components/usuario-form/usuario-form.component';
 import { UsuarioListComponent } from './components/usuario-list/usuario-list.component';
 import { UsuarioViewComponent } from './components/usuario-view/usuario-view.component';
 
-
-
 @NgModule({
   declarations: [UsuarioFormComponent, UsuarioListComponent, UsuarioViewComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    UsuarioRoutingModule
   ]
 })
 export class UsuarioModule { }
