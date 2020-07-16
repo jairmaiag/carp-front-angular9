ae33d9a57b8bc94752e5266450715e6054640911
jairmaiag <jairmaiag@gmail.com>
2020-07-16 18:20:20 -0300

    Criação do formulário de usuario

--- src/app/modules/usuario/components/usuario-form/usuario-form.component.ts
+++ src/app/modules/usuario/components/usuario-form/usuario-form.component.ts
@@ -1,4 +1,7 @@
+import { Global } from './../../global';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-usuario-form',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario-form.component.css']
 })
 export class UsuarioFormComponent implements OnInit {
+  id: any;
+  tituloComponente: String;
+  formulario: FormGroup;
 
-  constructor() { }
+  constructor(private global: Global, private router: Router, private activeRouter: ActivatedRoute) {
+    this.tituloComponente = `${this.global.inclusao}${this.global.titulo}`;
+  }
 
   ngOnInit(): void {
+    this.id = this.activeRouter.snapshot.paramMap.get('id');
+    if (this.id != null) {
+      this.tituloComponente = `${this.global.alteracao}${this.global.titulo}`;
+    }
+
+  }
+
+  cancelar() {
+    this.router.navigateByUrl('/usuario');
   }
 
 }
