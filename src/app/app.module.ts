ceb014a4cb7833aa97e2618f7cdc705dd5f44894
jairmaiag <jairmaiag@gmail.com>
2020-07-16 18:18:56 -0300

    Inclusão do ReactiveFormsModule para tratamento de formulários reativos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -21,6 +21,7 @@ import { ModalComponent } from './components/geral/modal/modal.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     AppRoutingModule
   ],
