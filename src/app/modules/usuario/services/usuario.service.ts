0efd509d76a4debfcca7676d7122a1767479e85b
jairmaiag <jairmaiag@gmail.com>
2020-07-16 18:19:27 -0300

    Criação dos métodos do serviço

--- src/app/modules/usuario/services/usuario.service.ts
+++ src/app/modules/usuario/services/usuario.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { Usuario } from './../models/usuario';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
   constructor() { }
+
+  getList(): Array<Usuario> {
+    return null;
+  }
+  getById(id: any): Usuario {
+    return null;
+  }
+  salve(usuario: Usuario) {
+
+  }
+  delete(id: any) {
+
+  }
 }
