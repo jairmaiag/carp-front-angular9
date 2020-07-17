a7e584a38c275d952b4804157882b71ee446b4ad
jairmaiag <jairmaiag@gmail.com>
2020-07-17 09:12:37 -0300

    Conversor para entidade usuários

--- src/app/modules/usuario/services/usuario.service.ts
+++ src/app/modules/usuario/services/usuario.service.ts
@@ -1,24 +1,46 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { take,tap } from 'rxjs/operators';
 import { Usuario } from './../models/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+  private readonly API = 'http://localhost/usuario';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getList(): Array<Usuario> {
-    return null;
+  getList() {
+    return this.http.get<Usuario[]>(this.API).pipe(tap(console.log));
   }
   getById(id: any): Usuario {
     return null;
   }
-  salve(usuario: Usuario) {
-
+  salvar(usuario: any) {
+    let retorno: any = this.http.post(this.API, this.converter(usuario)).pipe(take(1));
+    console.log(retorno);
+    return retorno;
+    //console.log(this.converter(usuario));
   }
   delete(id: any) {
 
+  }
+  converter(dados: any): Usuario {
+    if (!dados) {
+      return null;
+    }
+    let retorno: Usuario = new Usuario();
+    let ativo = dados?.ativo
+    ativo = ativo === null ? false : ativo;
+    retorno.id = dados?.id;
+    retorno.login = dados?.login;
+    retorno.ativo = ativo;
+    retorno.pessoa = {
+      nome: dados?.nome,
+      sobrenome: dados?.sobrenome,
+      ativo: ativo
+    }
+    return retorno;
   }
 }
