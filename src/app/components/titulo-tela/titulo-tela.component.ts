9d1c9e08cbcff7ed7644066c706f8e4a76235011
jairmaiag <jairmaiag@gmail.com>
2020-07-22 15:21:28 -0300

    Componente de título de tela

---
+++ src/app/components/titulo-tela/titulo-tela.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-titulo-tela',
+  templateUrl: './titulo-tela.component.html',
+  styleUrls: ['./titulo-tela.component.css']
+})
+export class TituloTelaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
