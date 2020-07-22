8486983bfb04f8b3c0f774ae4cec88c9edbbac00
jairmaiag <jairmaiag@gmail.com>
2020-07-22 15:23:38 -0300

    Conexão com o servidor

--- src/app/modules/usuario/services/usuario.service.ts
+++ src/app/modules/usuario/services/usuario.service.ts
@@ -1,46 +1,68 @@
+import { Paginacao } from './../../../models/paginacao';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { take,tap } from 'rxjs/operators';
+
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { environment } from './../../../../environments/environment.prod';
+import { Global } from './../global';
+import { Pessoa } from './../../../models/pessoa';
 import { Usuario } from './../models/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-  private readonly API = 'http://localhost/usuario';
+
+  private readonly API = `${environment.API}${Global.modulo}`;
 
   constructor(private http: HttpClient) { }
 
-  getList() {
-    return this.http.get<Usuario[]>(this.API).pipe(tap(console.log));
+  getList(): Observable<Paginacao> {
+    return this.http.get<Paginacao>(`${this.API}/paginacao`);
+    // return this.http.get<Paginacao>(`${this.API}/paginacao`).pipe(tap(console.log));
+    // return this.http.get<Usuario[]>(`${this.API}/paginacao`).pipe(tap(console.log));
   }
-  getById(id: any): Usuario {
-    return null;
-  }
-  salvar(usuario: any) {
-    let retorno: any = this.http.post(this.API, this.converter(usuario)).pipe(take(1));
-    console.log(retorno);
-    return retorno;
-    //console.log(this.converter(usuario));
-  }
-  delete(id: any) {
 
+  getById(id: any): Observable<Usuario> {
+    const url = `${this.API}/${id}`;
+    return this.http.get<Usuario>(url);
   }
+
+  salvar(usuario: any): Observable<Usuario> {
+    const entidade: Usuario = this.converter(usuario);
+    if (entidade.id) {
+      return this.http.put<Usuario>(this.API, entidade);
+    } else {
+      return this.http.post<Usuario>(this.API, entidade);
+    }
+  }
+
+  delete(UUId: any): Observable<Usuario> {
+    if (UUId == null) {
+      return;
+    }
+    const url = `${this.API}UUId/${UUId}`;
+    return this.http.delete<Usuario>(url);
+  }
+
   converter(dados: any): Usuario {
     if (!dados) {
       return null;
     }
     let retorno: Usuario = new Usuario();
+    let pessoa: Pessoa = new Pessoa();
     let ativo = dados?.ativo
     ativo = ativo === null ? false : ativo;
     retorno.id = dados?.id;
     retorno.login = dados?.login;
+    retorno.senha = dados?.senha;
     retorno.ativo = ativo;
-    retorno.pessoa = {
-      nome: dados?.nome,
-      sobrenome: dados?.sobrenome,
-      ativo: ativo
-    }
+    pessoa.nome = dados?.nome;
+    pessoa.sobrenome = dados?.sobrenome;
+    pessoa.ativo = ativo;
+    retorno.Pessoa = pessoa;
     return retorno;
   }
 }
