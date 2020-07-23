6705e88bfa445648e2e7e63ca2563248ca92d397
jairmaiag <jairmaiag@gmail.com>
2020-07-23 20:06:10 -0300

    Inclusão do componente Botao-Fechar

--- src/app/modules/geral/geral.module.ts
+++ src/app/modules/geral/geral.module.ts
@@ -5,12 +5,13 @@ import { ModalComponent } from './components/modal/modal.component';
 import { BotaoComponent } from './components/botao/botao.component';
 import { AcaoComponent } from './components/acao/acao.component';
 import { TituloTelaComponent } from './components/titulo-tela/titulo-tela.component';
+import { BotaoFecharComponent } from './components/botao-fechar/botao-fechar.component';
 
 @NgModule({
-  declarations: [ModalComponent, BotaoComponent, AcaoComponent, TituloTelaComponent],
+  declarations: [ModalComponent, BotaoComponent, AcaoComponent, TituloTelaComponent, BotaoFecharComponent],
   imports: [
     CommonModule
   ],
-  exports: [ModalComponent, BotaoComponent, AcaoComponent, TituloTelaComponent]
+  exports: [ModalComponent, BotaoComponent, AcaoComponent, TituloTelaComponent, BotaoFecharComponent]
 })
 export class GeralModule { }
