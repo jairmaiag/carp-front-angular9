c3c8f5f99ade85ba39f782d54a699ac65068318a
jairmaiag <jairmaiag@gmail.com>
2020-07-23 20:20:17 -0300

    Inclusão do componente botao novo no modulo

--- src/app/modules/geral/geral.module.ts
+++ src/app/modules/geral/geral.module.ts
@@ -6,12 +6,13 @@ import { BotaoComponent } from './components/botao/botao.component';
 import { AcaoComponent } from './components/acao/acao.component';
 import { TituloTelaComponent } from './components/titulo-tela/titulo-tela.component';
 import { BotaoFecharComponent } from './components/botao-fechar/botao-fechar.component';
+import { BotaoNovoComponent } from './components/botao-novo/botao-novo.component';
 
 @NgModule({
-  declarations: [ModalComponent, BotaoComponent, AcaoComponent, TituloTelaComponent, BotaoFecharComponent],
+  declarations: [ModalComponent, BotaoComponent, AcaoComponent, TituloTelaComponent, BotaoFecharComponent, BotaoNovoComponent],
   imports: [
     CommonModule
   ],
-  exports: [ModalComponent, BotaoComponent, AcaoComponent, TituloTelaComponent, BotaoFecharComponent]
+  exports: [ModalComponent, BotaoComponent, AcaoComponent, TituloTelaComponent, BotaoFecharComponent, BotaoNovoComponent]
 })
 export class GeralModule { }
