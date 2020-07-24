88118e41d757e7acacef86626c7e70e038ccb908
jairmaiag <jairmaiag@gmail.com>
2020-07-24 10:37:12 -0300

    Criação do componente de formulario no modulo de orçamento

---
+++ src/app/modules/orcamento/components/orcamento-form/orcamento-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orcamento-form',
+  templateUrl: './orcamento-form.component.html',
+  styleUrls: ['./orcamento-form.component.css']
+})
+export class OrcamentoFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
