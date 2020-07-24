f8a2bbea136567884145966f5243cfa985d9c86b
jairmaiag <jairmaiag@gmail.com>
2020-07-24 10:38:23 -0300

    Criação das rotas do módulo de orçamento com os novos componentes

--- src/app/modules/orcamento/orcamento-routing.module.ts
+++ src/app/modules/orcamento/orcamento-routing.module.ts
@@ -1,10 +1,15 @@
-import { OrcamentoComponent } from './orcamento.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { OrcamentoListComponent } from './components/orcamento-list/orcamento-list.component';
+import { OrcamentoFormComponent } from './components/orcamento-form/orcamento-form.component';
+import { OrcamentoViewComponent } from './components/orcamento-view/orcamento-view.component';
 
 const routes: Routes = [
-  { path: '', component: OrcamentoComponent }
+  { path: '', component: OrcamentoListComponent },
+  { path: 'new', component: OrcamentoFormComponent },
+  { path: ':uuid/view', component: OrcamentoViewComponent },
+  { path: ':uuid/edit', component: OrcamentoFormComponent }
 ];
 
 @NgModule({
