9b1370a5dde4167093cfe0bcb91e0e7e29740585
jairmaiag <jairmaiag@gmail.com>
2020-07-24 16:02:16 -0300

    Configuração para troca de nome da tela de formulário

--- src/app/modules/orcamento/components/orcamento-form/orcamento-form.component.ts
+++ src/app/modules/orcamento/components/orcamento-form/orcamento-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 import { OrcamentoService } from '../../services/orcamento.service';
 
@@ -12,8 +12,16 @@ export class OrcamentoFormComponent implements OnInit {
 
   titulo: String = OrcamentoService.tituloInclusao;
 
-  constructor(private servico: OrcamentoService) { }
-
+  constructor(private servico: OrcamentoService, private activeRouter: ActivatedRoute) {
+    let uuid = this.activeRouter.snapshot.paramMap.get('uuid')
+    if (uuid) {
+      this.titulo = OrcamentoService.tituloEdicao;
+      this.getByUUId(uuid);
+    }
+  }
+  getByUUId(uuid: string) {
+    this.servico.getByUUId(uuid);
+  }
   ngOnInit(): void {
   }
 
