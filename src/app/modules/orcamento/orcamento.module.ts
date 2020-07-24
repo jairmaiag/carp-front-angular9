e47df38a19be009ccad30ccc3bbd99ac9df97370
jairmaiag <jairmaiag@gmail.com>
2020-07-24 15:19:57 -0300

    Configuração do serviço OrcamentoService no módulo de orçamento

--- src/app/modules/orcamento/orcamento.module.ts
+++ src/app/modules/orcamento/orcamento.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GeralModule } from './../geral/geral.module';
+import { OrcamentoService } from './services/orcamento.service';
 import { OrcamentoRoutingModule } from './orcamento-routing.module';
 import { OrcamentoListComponent } from './components/orcamento-list/orcamento-list.component';
 import { OrcamentoFormComponent } from './components/orcamento-form/orcamento-form.component';
@@ -18,6 +19,7 @@ import { OrcamentoViewComponent } from './components/orcamento-view/orcamento-vi
     CommonModule,
     OrcamentoRoutingModule,
     GeralModule
-  ]
+  ],
+  providers: [OrcamentoService]
 })
 export class OrcamentoModule { }
