0c007d92b7b3a1ccf08a630d5efa971c37f70e83
jairmaiag <jairmaiag@gmail.com>
2020-07-24 15:12:53 -0300

    Inclusão do componente botaoDetalhar no modulo geral

--- src/app/modules/geral/geral.module.ts
+++ src/app/modules/geral/geral.module.ts
@@ -4,24 +4,24 @@ import { CommonModule } from '@angular/common';
 import { Constantes } from './services/constantes.service';
 import { ModalComponent } from './components/modal/modal.component';
 import { BotaoComponent } from './components/botao/botao.component';
-import { AcaoComponent } from './components/acao/acao.component';
 import { TituloTelaComponent } from './components/titulo-tela/titulo-tela.component';
 import { BotaoFecharComponent } from './components/botao-fechar/botao-fechar.component';
 import { BotaoNovoComponent } from './components/botao-novo/botao-novo.component';
 import { BotaoSalvarComponent } from './components/botao-salvar/botao-salvar.component';
 import { BotaoCancelarComponent } from './components/botao-cancelar/botao-cancelar.component';
 import { TelaComponent } from './components/tela/tela.component';
+import { BotaoDetalharComponent } from './components/botao-detalhar/botao-detalhar.component';
 
 @NgModule({
   declarations: [
     ModalComponent,
     BotaoComponent,
-    AcaoComponent,
     TituloTelaComponent,
     BotaoFecharComponent,
     BotaoNovoComponent,
     BotaoSalvarComponent,
     BotaoCancelarComponent,
+    BotaoDetalharComponent,
     TelaComponent
   ],
   imports: [
@@ -30,12 +30,12 @@ import { TelaComponent } from './components/tela/tela.component';
   exports: [
     ModalComponent,
     BotaoComponent,
-    AcaoComponent,
     TituloTelaComponent,
     BotaoFecharComponent,
     BotaoNovoComponent,
     BotaoSalvarComponent,
     BotaoCancelarComponent,
+    BotaoDetalharComponent,
     TelaComponent
   ],
   providers: [Constantes]
