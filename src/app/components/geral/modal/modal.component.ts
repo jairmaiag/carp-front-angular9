0ed5077c47017665316d96dc70164eedf3b99244
jairmaiag <jairmaiag@gmail.com>
2020-07-24 20:41:27 -0300

    Componente modal, ainda falta terminar

--- src/app/components/geral/modal/modal.component.ts
+++ src/app/components/geral/modal/modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-modal',
+  selector: 'modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
