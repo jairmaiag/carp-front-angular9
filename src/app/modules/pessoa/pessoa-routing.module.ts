a1c9de8d7c66c460a2524429fd9dca7b1b751659
jairmaiag <jairmaiag@gmail.com>
2020-07-25 13:24:08 -0300

    criação das rotas de new e de edit

--- src/app/modules/pessoa/pessoa-routing.module.ts
+++ src/app/modules/pessoa/pessoa-routing.module.ts
@@ -1,10 +1,13 @@
-import { PessoaListComponent } from './components/pessoa-list/pessoa-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PessoaFormComponent } from './components/pessoa-form/pessoa-form.component';
+import { PessoaListComponent } from './components/pessoa-list/pessoa-list.component';
 
 const routes: Routes = [
-  { path: '', component: PessoaListComponent }
+  { path: '', component: PessoaListComponent },
+  { path: 'new', component: PessoaFormComponent },
+  { path: ':uuid/edit', component: PessoaFormComponent }
 ];
 
 @NgModule({
