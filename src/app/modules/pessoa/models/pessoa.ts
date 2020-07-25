fe72d3a10cdacf900dfb3f4c2de6dabc324d407e
jairmaiag <jairmaiag@gmail.com>
2020-07-25 10:18:38 -0300

    Criação do model de pessoa

---
+++ src/app/modules/pessoa/models/pessoa.ts
@@ -0,0 +1,12 @@
+export class Pessoa {
+    id?: number;
+    UUId?: string;
+    nome?: string;
+    nomeMeio?: string;
+    sobrenome?: string;
+    nascimento?: Date;
+    sexo?: string;
+    cpf?: string;
+    rg?: string;
+    ativo: boolean = true;
+}
