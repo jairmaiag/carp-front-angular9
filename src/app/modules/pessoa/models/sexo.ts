4c0566c7da03a8b3f33786b8ffea5e4ed4bb3b99
jairmaiag <jairmaiag@gmail.com>
2020-07-25 18:14:27 -0300

    Criação da class sexo

---
+++ src/app/modules/pessoa/models/sexo.ts
@@ -0,0 +1,8 @@
+export class Sexo {
+    constructor(_id: string, _descricao: string) {
+        this.id = _id;
+        this.descricao = _descricao;
+    }
+    id: string;
+    descricao: string;
+}
