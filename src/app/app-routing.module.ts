6e82ad2f6db822a44dfc86788948b8ded1c36f2c
jairmaiag <jairmaiag@gmail.com>
2020-07-25 10:03:27 -0300

    Inclusão da rota de pessoa no módulo principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'pessoa', loadChildren: () => import('./modules/pessoa/pessoa.module').then(m => m.PessoaModule) },
   { path: 'produto', loadChildren: () => import('./modules/produto/produto.module').then(m => m.ProdutoModule) },
   { path: 'usuario', loadChildren: () => import('./modules/usuario/usuario.module').then(m => m.UsuarioModule) },
   { path: 'orcamento', loadChildren: () => import('./modules/orcamento/orcamento.module').then(m => m.OrcamentoModule) }
