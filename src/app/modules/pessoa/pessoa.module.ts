935d65d3d0df53e33aba765190099e8deef6ce37
jairmaiag <jairmaiag@gmail.com>
2020-07-25 09:57:14 -0300

    Inclusão do componente de list pessoa no módulo de pessoa

--- src/app/modules/pessoa/pessoa.module.ts
+++ src/app/modules/pessoa/pessoa.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PessoaRoutingModule } from './pessoa-routing.module';
+import { PessoaListComponent } from './components/pessoa-list/pessoa-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PessoaListComponent],
   imports: [
     CommonModule,
     PessoaRoutingModule
