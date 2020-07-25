2426b95c7af94ef0c2f9cedf4c4b5dfa370094a0
jairmaiag <jairmaiag@gmail.com>
2020-07-25 12:54:52 -0300

    Inclusao do modulo FormsModule para

--- src/app/modules/pessoa/pessoa.module.ts
+++ src/app/modules/pessoa/pessoa.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";;
 import { HttpClientModule } from "@angular/common/http";
 
 import { PessoaRoutingModule } from './pessoa-routing.module';
@@ -14,6 +15,7 @@ import { PessoaFormComponent } from './components/pessoa-form/pessoa-form.compon
   imports: [
     CommonModule,
     HttpClientModule,
+    FormsModule,
     PessoaRoutingModule,
     GeralModule
   ],
