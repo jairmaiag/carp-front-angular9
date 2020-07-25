797c3a1708806d7579b8474141c052278940e787
jairmaiag <jairmaiag@gmail.com>
2020-07-25 10:25:28 -0300

    Criação das constantes de títulos no serviço de pessoa

--- src/app/modules/pessoa/services/pessoa.service.ts
+++ src/app/modules/pessoa/services/pessoa.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 
+import { Constantes } from '../../../modules/geral/services/constantes.service';
 @Injectable({
   providedIn: 'root'
 })
 export class PessoaService {
+
+  static modulo: string = 'pessoa';
+  static titulo: string = 'Pessoa';
+  static tituloListagem: string = `${Constantes.listagem}${PessoaService.titulo}`;
+  static tituloInclusao: string = `${Constantes.inclusao}${PessoaService.titulo}`;
+  static tituloEdicao: string = `${Constantes.alteracao}${PessoaService.titulo}`;
+  static tituloDetalhe: string = `${Constantes.detalhe}${PessoaService.titulo}`;
 
   constructor() { }
 }
