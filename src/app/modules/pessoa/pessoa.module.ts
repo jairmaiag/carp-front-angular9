21bad49f96b1a8e01e4c54c7288a698a2d800503
jairmaiag <jairmaiag@gmail.com>
2020-07-25 10:45:28 -0300

    Importação e configuração do módulo HttpClientModule no módulo de pessoa

--- src/app/modules/pessoa/pessoa.module.ts
+++ src/app/modules/pessoa/pessoa.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from "@angular/common/http";
 
 import { PessoaRoutingModule } from './pessoa-routing.module';
 import { GeralModule } from './../geral/geral.module';
@@ -11,6 +12,7 @@ import { PessoaService } from './services/pessoa.service';
   declarations: [PessoaListComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     PessoaRoutingModule,
     GeralModule
   ],
