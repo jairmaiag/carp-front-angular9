06352a2b4edc5686fa7342ccd935583b612ccdba
jairmaiag <jairmaiag@gmail.com>
2020-07-25 09:59:26 -0300

    Configuração do serviço no módulo de pessoa

--- src/app/modules/pessoa/pessoa.module.ts
+++ src/app/modules/pessoa/pessoa.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PessoaRoutingModule } from './pessoa-routing.module';
+
 import { PessoaListComponent } from './components/pessoa-list/pessoa-list.component';
+import { PessoaService } from './services/pessoa.service';
 
 
 @NgModule({
@@ -10,6 +12,7 @@ import { PessoaListComponent } from './components/pessoa-list/pessoa-list.compon
   imports: [
     CommonModule,
     PessoaRoutingModule
-  ]
+  ],
+  providers: [PessoaService]
 })
 export class PessoaModule { }
