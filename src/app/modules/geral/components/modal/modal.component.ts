cbc7c941b7e943cee45b022512508c19aa3771d8
jairmaiag <jairmaiag@gmail.com>
2020-07-29 17:21:18 -0300

    Correção do modal - Ainda falta terminar

--- src/app/modules/geral/components/modal/modal.component.ts
+++ src/app/modules/geral/components/modal/modal.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
-  selector: 'modal',
+  selector: 'modal-default',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  modalRef: BsModalRef;
+  message: string;
 
-  constructor() { }
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit(): void {
+  }
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+  confirm(): void {
+    this.message = 'Confirmed!';
+    this.modalRef.hide();
+  }
+
+  decline(): void {
+    this.message = 'Declined!';
+    this.modalRef.hide();
   }
 
 }
