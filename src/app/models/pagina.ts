a9ede1d5394b311e85737476b74ce0169a4a5c97
jairmaiag <jairmaiag@gmail.com>
2020-08-14 11:09:37 -0300

    Criação do método statico getInstance

--- src/app/models/pagina.ts
+++ src/app/models/pagina.ts
@@ -1,9 +1,21 @@
 export class Pagina {
-    fieldName: string;
-    next: number;
-    previous: number;
-    previousId: number;
-    size: number;
-    total: number;
-    totalRows: number;
+    fieldName: string = "id";
+    next: number = 0;
+    previous: number = 0;
+    previousId: number = 0;
+    size: number = 10;
+    total: number = 0;
+    totalRows: number = 0;
+
+    static getInstance(): Pagina {
+        let retorno = new Pagina();
+        retorno.fieldName = "id";
+        retorno.next = 0;
+        retorno.previous = 0;
+        retorno.previousId = 0;
+        retorno.size = 10;
+        retorno.total = 0;
+        retorno.totalRows = 0;
+        return retorno;
+    }
 }
